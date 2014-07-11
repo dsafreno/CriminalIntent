d4f72dff8d78c349587b1c1823d2a3ce106e6d7a
Doug Safreno <doug@asana.com>
2014-07-11 11:58:59 -0700

    Chapter 9

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -2,11 +2,12 @@ package com.bignerdranch.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-
-public class CrimeActivity extends SingleFragmentActivity {
-
+/**
+ * Created by asana341 on 7/10/14.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
